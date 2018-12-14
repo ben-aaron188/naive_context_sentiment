c15eea6c106d47a8f1c807c6be179337cf7dcca3
ben-aaron188 <ben-aaron188@users.noreply.github.com>
2018-12-14 13:26:08 +0100

    added concreteness, profanity and racial slurs as dimensions

--- ncs.R
+++ ncs.R
@@ -6,11 +6,14 @@
 ### OUTPUT: dataframe with scaled sentiments of equal size
 ###############################################################################
 
+print('For external lexicon-databases, please set the current wd to the parent of the ncs function (e.g. the GitHub repo)')
+
 #1. PREPROCESSING
 ##accepts string input, constructs context cluster, and weighs sentiment
 ##INPUT: string input + params on valence shifter weights and cluster size
 ##OUTPUT: vector of transformed sentiments (or: dataframe - use this for debugging)
 ncs_preprocess = function(string_input
+                          , lexicon_ = 'sentiment'
                           , cluster_lower_ = 2
                           , cluster_upper_ = 2
                           , weight_negator_ = -1
@@ -41,73 +44,130 @@ ncs_preprocess = function(string_input
   text.raw = data.table(text = mod_string
                         , index = 1:length(mod_string))
 
-  #load hash tables OPTIMIZE!
-  hash.sentiment = lexicon::hash_sentiment_jockers_rinker
-  hash.valence_shifters = lexicon::hash_valence_shifters
-
-  #locate sentiments
-  text.sentiment = merge(text.raw
-                         , hash.sentiment
-                         , by.x = 'text'
-                         , by.y = 'x'
-                         , all.x = TRUE)
-  text.sentiment = text.sentiment[order(index),]
-
-  #locate valence shifters
-  text.valence_shifters = merge(text.sentiment
-                                , hash.valence_shifters
-                                , by.x = 'text'
-                                , by.y = 'x'
-                                , all.x = TRUE)
-  text.valence_shifters = text.valence_shifters[order(index),]
-
-  text.table = text.valence_shifters
-
-  #recode valence shifters
-  names(text.table)[3:4] = c('sentiment'
-                             , 'valence'
-  )
-
-  #key to valence shifters:
-  #1 = negator
-  #2 = amplifier
-  #3 = deamplifier
-  #4 = adversative conjunction
-  text.table$weights = ifelse(is.na(text.table$valence), 1,
-                              ifelse(text.table$valence == 1, weight_negator_,
-                                     ifelse(text.table$valence == 2, weight_amplifier_,
-                                            ifelse(text.table$valence == 3, weight_deamplifier_,
-                                                   weight_advcon_))))
-
-  text.table$sentiment_score = text.table$sentiment
-  text.table$sentiment_score[is.na(text.table$sentiment_score)] = 0
-  text.table$sentiment_score_mod = text.table$sentiment_score
-
-  for(i in 1:length(text.table$sentiment_score)){
-    if(text.table$sentiment_score[i] != 0){
-      cluster_boundary_lower = ifelse((i-cluster_lower_) > 0, (i-cluster_lower_), 1)
-      cluster_boundary_upper = ifelse((i+cluster_upper_) < length(text.table$sentiment_score), (i+cluster_upper_), length(text.table$sentiment_score))
-      a = text.table$weights[cluster_boundary_lower:cluster_boundary_upper]
-      a[(1+cluster_lower_)] = text.table$sentiment_score[i]
-      text.table$sentiment_score_mod[i] = prod(a, na.rm = T)
-
-      if(verbose == T){
-        print(paste('sentiment change for "', text.table$text[i], '": ',  text.table$sentiment_score[i], ' --> ', prod(a), sep=""))
-
+  
+  #set lexicon selection
+  if(lexicon_ == 'sentiment'){
+    
+    #load hash tables OPTIMIZE!
+    hash.sentiment = lexicon::hash_sentiment_jockers_rinker
+    hash.valence_shifters = lexicon::hash_valence_shifters
+    
+    #locate sentiments
+    text.sentiment = merge(text.raw
+                           , hash.sentiment
+                           , by.x = 'text'
+                           , by.y = 'x'
+                           , all.x = TRUE)
+    text.sentiment = text.sentiment[order(index),]
+    
+    #locate valence shifters
+    text.valence_shifters = merge(text.sentiment
+                                  , hash.valence_shifters
+                                  , by.x = 'text'
+                                  , by.y = 'x'
+                                  , all.x = TRUE)
+    text.valence_shifters = text.valence_shifters[order(index),]
+    
+    text.table = text.valence_shifters
+    
+    #recode valence shifters
+    names(text.table)[3:4] = c('sentiment'
+                               , 'valence'
+    )
+    #key to valence shifters:
+    #1 = negator
+    #2 = amplifier
+    #3 = deamplifier
+    #4 = adversative conjunction
+    text.table$weights = ifelse(is.na(text.table$valence), 1,
+                                ifelse(text.table$valence == 1, weight_negator_,
+                                       ifelse(text.table$valence == 2, weight_amplifier_,
+                                              ifelse(text.table$valence == 3, weight_deamplifier_,
+                                                     weight_advcon_))))
+    
+    text.table$score = text.table$sentiment
+    text.table$score[is.na(text.table$score)] = 0
+    text.table$score_mod = text.table$score
+    
+    for(i in 1:length(text.table$score)){
+      if(text.table$score[i] != 0){
+        cluster_boundary_lower = ifelse((i-cluster_lower_) > 0, (i-cluster_lower_), 1)
+        cluster_boundary_upper = ifelse((i+cluster_upper_) < length(text.table$score), (i+cluster_upper_), length(text.table$score))
+        a = text.table$weights[cluster_boundary_lower:cluster_boundary_upper]
+        a[(1+cluster_lower_)] = text.table$score[i]
+        text.table$score_mod[i] = prod(a, na.rm = T)
+        
+        if(verbose == T){
+          print(paste('sentiment change for "', text.table$text[i], '": ',  text.table$score[i], ' --> ', prod(a), sep=""))
+        }
+        
       }
-
     }
+  } else if (lexicon_ == 'concreteness'){
+    
+    load('./lexicon_data/ncs_lexicon_concreteness.RData')
+    
+    hash.lexicon = lexicon.concreteness
+    text.hash_merged = merge(text.raw
+                           , hash.lexicon
+                           , by.x = 'text'
+                           , by.y = 'word'
+                           , all.x = TRUE)
+    text.hash_merged = text.hash_merged[order(index),]
+    
+    text.table = text.hash_merged
+    
+    text.table$score = text.table$value
+    text.table$score[is.na(text.table$score)] = 0
+    text.table$score_mod = text.table$score
+    
+  } else if (lexicon_ == 'racialslurs'){
+    
+    load('./lexicon_data/ncs_lexicon_racialslurs.RData')
+    
+    hash.lexicon = lexicon.racialslurs
+    text.hash_merged = merge(text.raw
+                             , hash.lexicon
+                             , by.x = 'text'
+                             , by.y = 'word'
+                             , all.x = TRUE)
+    text.hash_merged = text.hash_merged[order(index),]
+    
+    text.table = text.hash_merged
+    
+    text.table$score = text.table$value
+    text.table$score[is.na(text.table$score)] = 0
+    text.table$score_mod = text.table$score
+    
+  } else if (lexicon_ == 'profanity'){
+    
+    load('./lexicon_data/ncs_lexicon_profanity.RData')
+    
+    hash.lexicon = lexicon.profanity
+    text.hash_merged = merge(text.raw
+                             , hash.lexicon
+                             , by.x = 'text'
+                             , by.y = 'word'
+                             , all.x = TRUE)
+    text.hash_merged = text.hash_merged[order(index),]
+    
+    text.table = text.hash_merged
+    
+    text.table$score = text.table$value
+    text.table$score[is.na(text.table$score)] = 0
+    text.table$score_mod = text.table$score
+    
   }
-
+  
   if(return_df == T){
     new_text.table = text.table
   } else if(return_df == F){
-    new_text.table = text.table$sentiment_score_mod
+    new_text.table = text.table$score_mod
   }
 
-  modified_sentiment_indices = which(text.table$sentiment_score != text.table$sentiment_score_mod)
-  prop_changed = round(length(modified_sentiment_indices)/length(text.table$sentiment_score_mod), 2)
-  print(paste('Proportion of total modified sentiments: ', prop_changed, sep=""))
+  # modified_sentiment_indices = which(text.table$sentiment_score != text.table$sentiment_score_mod)
+  # prop_changed = round(length(modified_sentiment_indices)/length(text.table$sentiment_score_mod), 2)
+  # print(paste('Proportion of total modified sentiments: ', prop_changed, sep=""))
 
   return(new_text.table)
 }
@@ -120,6 +180,7 @@ ncs_preprocess = function(string_input
 ##OUTPUT: dataframe of dims bins*length(txt_input_col)
 ncs_full = function(txt_input_col
                     , txt_id_col
+                    , lexicon = 'sentiment'
                     , low_pass_filter_size = 5
                     , transform_values = T
                     , normalize_values = F
@@ -139,63 +200,90 @@ ncs_full = function(txt_input_col
   currentwd = getwd()
   t1 = Sys.time()
 
-  if(weight_negator < weight_deamplifier){
-    print('###################################################')
-    print(paste('Initialized ____'))
-    print(paste('Cluster size: -', cluster_lower, " : +", cluster_upper, sep=""))
-    print(paste('Negator weight: ', weight_negator, sep=""))
-    print(paste('Amplifier weight: ', weight_amplifier, sep=""))
-    print(paste('De-amplifier weight: ', weight_deamplifier, sep=""))
-    print(paste('Adv. conj. weight: ', weight_advcon, sep=""))
-    print('###################################################')
-  } else {
-    print('WARNING: Negator weight must be lower than deamplifier weight')
-  }
-
-
-  txt_col = txt_input_col
-  id_col = txt_id_col
-  empty_matrix = matrix(data = 0
-                        , nrow = bins
-                        , ncol = length(txt_col)
-  )
-  for(i in 1:length(txt_col)){
-    if(length(unlist(str_split(txt_col[i], ' '))) >= min_tokens) {
-      print(paste('Performing NCS extraction: ', id_col[i], sep=""))
-      print(paste('- - - - - - - - - - ', i, '/', length(id_col),  sep=""))
-      a = ncs_preprocess(string_input = txt_col[i]
-                         , cluster_lower_ = cluster_lower
-                         , cluster_upper_ = cluster_upper
-                         , weight_negator_ = weight_negator
-                         , weight_amplifier_ = weight_amplifier
-                         , weight_deamplifier_ = weight_deamplifier
-                         , weight_advcon_ = weight_advcon
-                         , return_df = F
-                         , verbose = verbose)
-      text.scored = a
-      text.scored_binned = get_dct_transform(text.scored
-                                             , x_reverse_len=bins
-                                             , low_pass_size = low_pass_filter_size
-                                             , scale_range = transform_values
-                                             , scale_vals = normalize_values)
-      empty_matrix[, i] = text.scored_binned
-      print('==================== NEXT ====================')
-    } else {
-      empty_matrix[, i] = rep(NA, bins)
+  if((lexicon %in% c('sentiment', 'concreteness', 'racialslurs', 'profanity'))){
+    
+    if(lexicon == 'sentiment'){
+      if(weight_negator < weight_deamplifier){
+        print('###################################################')
+        print(paste('Initialized ____'))
+        print(paste('Cluster size: -', cluster_lower, " : +", cluster_upper, sep=""))
+        print(paste('Negator weight: ', weight_negator, sep=""))
+        print(paste('Amplifier weight: ', weight_amplifier, sep=""))
+        print(paste('De-amplifier weight: ', weight_deamplifier, sep=""))
+        print(paste('Adv. conj. weight: ', weight_advcon, sep=""))
+        print('###################################################')
+      } else {
+        print('WARNING: Negator weight must be lower than deamplifier weight')
+      }  
+    } else if(lexicon == 'concreteness'){
+      print('###################################################')
+      print(paste('Initialized ____'))
+      print(paste('Using concreteness lexicon'))
+      print('###################################################')
+    } else if(lexicon == 'racialslurs'){
+      print('###################################################')
+      print(paste('Initialized ____'))
+      print(paste('Using racial slurs lexicon'))
+      print('###################################################')
+    } else if(lexicon == 'profanity'){
+      print('###################################################')
+      print(paste('Initialized ____'))
+      print(paste('Using profanity lexicon'))
+      print('###################################################')
     }
-
+    
+    txt_col = txt_input_col
+    id_col = txt_id_col
+    empty_matrix = matrix(data = 0
+                          , nrow = bins
+                          , ncol = length(txt_col)
+    )
+    for(i in 1:length(txt_col)){
+      if(length(unlist(str_split(txt_col[i], ' '))) >= min_tokens) {
+        print(paste('Performing NCS extraction: ', id_col[i], sep=""))
+        print(paste('- - - - - - - - - - ', i, '/', length(id_col),  sep=""))
+        a = ncs_preprocess(string_input = txt_col[i]
+                           , lexicon_ = lexicon
+                           , cluster_lower_ = cluster_lower
+                           , cluster_upper_ = cluster_upper
+                           , weight_negator_ = weight_negator
+                           , weight_amplifier_ = weight_amplifier
+                           , weight_deamplifier_ = weight_deamplifier
+                           , weight_advcon_ = weight_advcon
+                           , return_df = F
+                           , verbose = verbose)
+        text.scored = a
+        text.scored_binned = get_dct_transform(text.scored
+                                               , x_reverse_len=bins
+                                               , low_pass_size = low_pass_filter_size
+                                               , scale_range = transform_values
+                                               , scale_vals = normalize_values)
+        empty_matrix[, i] = text.scored_binned
+        print('==================== NEXT ====================')
+      } else {
+        empty_matrix[, i] = rep(NA, bins)
+      }
+      
+    }
+    final_df = as.data.frame(empty_matrix)
+    colnames(final_df) = txt_id_col
+    t2 = Sys.time()
+    print(t2-t1)
+    setwd(currentwd)
+    return(final_df)
+    
+  } else {
+    print('Error: select one of the following "lexicon" parameters: sentiment, concreteness, racialslurs, profanity')
   }
-  final_df = as.data.frame(empty_matrix)
-  colnames(final_df) = txt_id_col
-  t2 = Sys.time()
-  print(t2-t1)
-  setwd(currentwd)
-  return(final_df)
+  
 }
 
 #end of full
 
-#3. USAGE EXAMPLE
+#3. Changelog
+# 14 Dec. 2018: added new dimensions with lexicon parameter
+
+#4. USAGE EXAMPLE
 ##for texts from source, use the function: https://github.com/ben-aaron188/r_helper_functions/blob/master/txt_df_from_dir.R
 # data = data.frame('text' = character(3)
 #                   , 'text_id' = character(3))
@@ -206,6 +294,7 @@ ncs_full = function(txt_input_col
 #
 # ncs_full(txt_input_col = data$text
 #          , txt_id_col = data$text_id
+#          , lexicon = 'sentiment'
 #          , low_pass_filter_size = 5
 #          , transform_values = T
 #          , normalize_values = F
